77ca09ab15cfc452057a1235298fc89cfae10b40
unknown <dch@uoregon.edu>
2023-04-23 12:47:35 -0700

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -7,23 +7,34 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.email
 
+class FlexibleDateField(models.DateTimeField):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        if isinstance(value, str):
+            # Try to parse the input string using multiple formats
+            try_formats = ['%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M', '%Y-%m-%d']
+            for format in try_formats:
+                try:
+                    value = datetime.strptime(value, format)
+                    break
+                except ValueError:
+                    pass
+        return super().get_prep_value(value)
+
 class TimeSeriesSetMetadata(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     application_domains = models.CharField(max_length=255)
-    keywords = models.CharField(max_length=255)    min_length = models.IntegerField()
-    likes = models.IntegerField() // maybe?
+    keywords = models.CharField(max_length=255)
+    min_length = models.IntegerField()
+    likes = models.IntegerField()
     num_time_series = models.IntegerField()
-    componentsTS = [array of TS in set]
     contributors = models.CharField(max_length=255)
     related_paper_reference = models.CharField(max_length=255)
     related_paper_link = models.URLField()
 
-class ForecastingTask(models.Model):
-    metadata = models.ForeignKey(TimeSeriesMetadata, on_delete=models.CASCADE)
-    start_datetime = FlexibleDateField()
-    num_forecasts = models.IntegerField()
-    
 class TimeSeriesMetadata(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -40,18 +51,7 @@ class TimeSeriesData(models.Model):
     vectorIndex = models.IntegerField()
     metadata = models.ForeignKey(TimeSeriesMetadata, on_delete=models.CASCADE, db_index=True)
 
-class FlexibleDateField(models.DateTimeField):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def get_prep_value(self, value):
-        if isinstance(value, str):
-            # Try to parse the input string using multiple formats
-            try_formats = ['%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M', '%Y-%m-%d'] // or whatever
-            for format in try_formats:
-                try:
-                    value = datetime.strptime(value, format)
-                    break
-                except ValueError:
-                    pass
-        return super().get_prep_value(value)
+class ForecastingTask(models.Model):
+    metadata = models.ForeignKey(TimeSeriesMetadata, on_delete=models.CASCADE)
+    start_datetime = FlexibleDateField()
+    num_forecasts = models.IntegerField()
